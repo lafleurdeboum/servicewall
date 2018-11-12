5b3fcc600e1d617a6247c5b55be3493dbb2336d0
lafleurdeboum <lafleur@boum.org>
2018-11-13 00:45:11 +0100

    updated version numbers

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ here = path.abspath(path.dirname(__file__))
 print("included packages : %s" %
         setuptools.find_packages(exclude=['scriptlets']))
 name="servicewall"
-version = "0.3.1"
+version = "0.3.2"
 
 setuptools.setup(
     name=name,
