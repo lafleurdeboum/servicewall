e0f7c91e31ed93b3cefbef0cbf32ff8e6b509d5f
lafleurdeboum <lafleur@boum.org>
2018-11-29 15:09:56 +0100

    using namedtuple in service_helpers.py
    and outputting service.ports as a range.
    
    Let's take the example of chat.ufw_app ; not shure if we should return the port
    list as "chat" or separated services in ["AIM", "Bonjour", "MSN", ...]. At the
    moment we do the latter.

--- servicewall/service_helpers.py
+++ servicewall/service_helpers.py
@@ -1,48 +1,45 @@
 from os import scandir
+from collections import namedtuple
 
 
 def split_port_def(port_def):
-    ports_list = []
-    # In the worst case, port_def is "13:15,124/udp|120:122/tcp"
-    # Which should mean 13 to 15 and 124, all udp, and 120 to 122 tcp
+    ports_dict = {"udp": [], "tcp": []}
+    PortDef = namedtuple("ports", "udp tcp")
+
+    # In the worst case, port_def is "13:15,124/udp|120:122"
+    # Which should mean 13 to 15 and 124, all udp, and 120 to 122 both udp and tcp
     try:
-        port_subdefs = port_def.split("|")
+        proto_subdefs = port_def.split("|")
     except ValueError:
-        port_subdefs = (port_def, )
+        proto_subdefs = (port_def, )
 
     # first port_subdef is "13:15,124,128/udp"
-    for port_subdef in port_subdefs:
+    for proto_subdef in proto_subdefs:
         try:
-            port_subdef, proto = port_subdef.split("/")
+            port_subdef, proto_def = proto_subdef.split("/")
+            protos = (proto_def, )
         except ValueError:
-            # We have a port but no proto ; that's OK.
-            # Empty proto means any of udp or tcp.
-            proto = ""
+            port_subdef = proto_subdef
+            protos = ("udp", "tcp")
 
-        # first port_subdef is now "13:15,124,128" : proto is "udp"
+        # first port_subdef is now "13:15,124,128" : protos is ("udp", )
         try:
-            ports = port_subdef.split(",")
+            port_list = port_subdef.split(",")
         except ValueError:
-            ports = (port_subdef, )
+            port_list = [ port_subdef ]
 
-        # ports is ("13:15", "124", "128")
-        for port in ports:
-            # port is a string ; let's check if this str is a number
-            if port.isalnum():
-                ports_list += ((int(port), proto), )
-            else:
-                # It's a port range, spelled ie "100:104".
-                ports_start, ports_end = port.split(":")
-                for port_range_item in range(int(ports_start), int(ports_end)+1):
-                    ports_list += ((port_range_item, proto), )
+        # port_list is ("13:15", "124", "128"). Port ranges can be very wide,
+        # yielding 10000 entries. We'll keep them as ranges.
+        for proto in protos:
+            ports_dict[proto].append(*port_list)
 
-    return tuple(ports_list)
+    return PortDef(ports_dict["udp"], ports_dict["tcp"])
 
 
 def scan_service_definitions(definitions_dir):
     '''scan service definitions in the list given by jhansonxi'''
-    # Files in the following dir define one or more protocols and
-    # their portsi. Each file comes with the following format :
+    # Files in the definitions dir define one or more protocols and
+    # their ports. Each file comes with the following format :
     #
     # [NAME_ONE]
     # title=EXPANDED NAME
@@ -56,6 +53,7 @@ def scan_service_definitions(definitions_dir):
 
     filelist = scandir(definitions_dir)
     services = {}
+    ServiceDef = namedtuple("service", "title description ports categories reference")
     for filename in iter(filelist):
         print("processing %s" % filename.path)
         with open(filename.path, "r") as fd:
@@ -67,18 +65,28 @@ def scan_service_definitions(definitions_dir):
                     continue
                 service_desc = service.split("\n")
                 service_name = service_desc[0].strip("[]")
+                print("  -> %s" % service_name)
 
                 # get rid of empty strings :
                 for i in range(service_desc.count("")):
                     service_desc.remove("")
 
                 # services['NAME_ONE'] will return a dict with its parameters :
-                services[service_name] = {
+                service_def = {
                     key: value for (key, value) in
                         ( item.split("=", 1) for item in iter(service_desc[1:]) )
                 }
-                port_def = split_port_def(services[service_name]["ports"])
-                services[service_name]["ports"] = port_def
+                if not "reference" in service_def:
+                    service_def["reference"] = ""
+                service_def["ports"] = split_port_def(service_def["ports"])
+                s = ServiceDef(
+                        service_def["title"],
+                        service_def["description"],
+                        service_def["ports"],
+                        service_def["categories"],
+                        service_def["reference"],
+                )
+                services[service_name] = s
 
     return services
 
