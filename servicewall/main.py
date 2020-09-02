8ec57599b7e61550fd8d4f690ba1d0672f6e3612
lafleur <lafleur@qi>
2020-09-02 14:55:07 +0200

    support for systemd fallback + ServiceWall.enable improvement
    If systemd is not found the process falls back to saving the state
    (enabled or not) to /etc/servicewall/config.cfg . Also,
    ServiceWall.enable will not start the service while enabling it.
    And code rearragements (accessory funcs at the end).

--- servicewall/main.py
+++ servicewall/main.py
@@ -90,10 +90,10 @@ class ServiceWall(statefulfirewall.StateFulFireWall):
             self.subnetwork = False
 
 
-    def start(self, check_hook=True, **args):
+    def start(self, should_check_hook=True, **args):
         """Will load a set of rules from self.realm_defs .
         """
-        if check_hook:
+        if should_check_hook:
             self._enable_hook()
         if self.realm_id not in self.realm_defs:
             # If we don't have a realm definition, load "ServiceWall:default"
@@ -103,66 +103,40 @@ class ServiceWall(statefulfirewall.StateFulFireWall):
         # Commits the table if relevant, and brings other rules in :
         super().start(**args)
 
-    def stop(self, check_hook=True):
-        if check_hook:
+    def stop(self, should_check_hook=True):
+        if should_check_hook:
             self._disable_hook()
         super().stop()
 
     def reload(self):
-        self.stop(check_hook=False)
-        self.start(check_hook=False)
+        self.stop(should_check_hook=False)
+        self.start(should_check_hook=False)
         print("%s reloaded" % self.identifier)
     
     def enable(self):
-        self._enable_in_systemd()
+        try:
+            self._enable_in_systemd()
+        except AssertionError:
+            with open(self.config_file, 'w+') as fd:
+                fd.write('{ "state": "enabled" }')
 
     def disable(self):
-        self._disable_in_systemd()
+        try:
+            self._disable_in_systemd()
+        except AssertionError:
+            with open(self.config_file, 'w+') as fd:
+                fd.write('{ "state": "disabled" }')
 
-    def _enable_hook(self):
-        """Create a link in the network dispatcher pointing to the event triggerer.
-        """
-        if not os.path.exists(self.lib_dir):    # Should be installed by setup.py .
-            raise SystemExit("Could not find %s in %s. Check your installation !" %
-                    (dispatcher_toggler, self.lib_dir))
-        # We will only mark as enabled if we can link to a network dispatcher :
-        linked = False
-        for dispatcher, dst_dir in self.dispatchers.items():
-            if not os.path.exists(dst_dir):
-                # Keep going with the next dispatcher.
-                continue
-            if os.path.exists(dst_dir + self.dispatcher_toggler):
-                print("%s dispatcher was already linked" % dispatcher)
-                linked = True
-            else:
-                print("%s dispatcher link created" % dispatcher)
-                # symlink pointing to src in dst_dir
-                os.symlink(self.lib_dir + self.dispatcher_toggler, dst_dir + self.dispatcher_toggler)
-                linked = True
-        if not linked:
-            raise SystemExit("Could not link to any network event dispatcher. "
-                    "You apparently aren't running neither Network Manager nor "
-                    "systemd-networkd with networkd-dispatcher. You'll need one "
-                    "of those to run this as it relies on them to fire the "
-                    "network change events.")
-
-    def _disable_hook(self):
-        """Destroy the link in the network dispatcher pointing to the event triggerer.
-        """
-        for dispatcher, target in self.dispatchers.items():
-            # DEBUG This test would fail on a broken link :
-            if os.path.exists(target + self.dispatcher_toggler):
-                os.remove(target + self.dispatcher_toggler)
-                print("%s dispatcher link destroyed" % dispatcher)
-            else:
-                # Report missing link only if dir is present
-                if os.path.exists(target):
-                    print("there was no %s dispatcher link" % dispatcher)
-
-    def save_rules(self):
-        with open(self.realm_defs_dict, "w") as fd:
-            json.dump(self.realm_defs, fd)
-        print("saved realm defs to config")
+    def is_enabled(self):
+        try:
+            status = self._systemctl('is-enabled')
+        except AssertionError:
+            with open(self.config_file, 'r') as fd:
+                config = json.load(fd)
+                status = config["state"]
+        if status != 'enabled':
+            return False
+        return True
 
     def allow_service(self, service_name, scope="local", realm=None):
         if service_name not in self.service_defs:
@@ -248,6 +222,10 @@ class ServiceWall(statefulfirewall.StateFulFireWall):
                 self.del_rule(service_name, self.input_chain)
                 #self.input_chain.delete_rule(rule)
 
+    def save_rules(self):
+        with open(self.realm_defs_dict, "w") as fd:
+            json.dump(self.realm_defs, fd)
+        print("saved realm defs to config")
 
     def list_services_in(self):
         """Lists services for which we have allowed ports.
@@ -269,23 +247,55 @@ class ServiceWall(statefulfirewall.StateFulFireWall):
                         services_list.append(service_name)
         return services_list
 
-    def is_enabled(self):
-        status = self._systemctl('is-enabled')
-        if status != b'enabled':
-            return False
-        return True
+    def _enable_hook(self):
+        """Create a link in the network dispatcher pointing to the event triggerer.
+        """
+        if not os.path.exists(self.lib_dir):    # Should be installed by setup.py .
+            raise SystemExit("Could not find %s in %s. Check your installation !" %
+                    (dispatcher_toggler, self.lib_dir))
+        # We will only mark as enabled if we can link to a network dispatcher :
+        linked = False
+        for dispatcher, dst_dir in self.dispatchers.items():
+            if not os.path.exists(dst_dir):
+                # Keep going with the next dispatcher.
+                continue
+            if os.path.exists(dst_dir + self.dispatcher_toggler):
+                print("%s dispatcher was already linked" % dispatcher)
+                linked = True
+            else:
+                print("%s dispatcher link created" % dispatcher)
+                # symlink pointing to src in dst_dir
+                os.symlink(self.lib_dir + self.dispatcher_toggler, dst_dir + self.dispatcher_toggler)
+                linked = True
+        if not linked:
+            raise SystemExit("Could not link to any network event dispatcher. "
+                    "You apparently aren't running neither Network Manager nor "
+                    "systemd-networkd with networkd-dispatcher. You'll need one "
+                    "of those to run this as it relies on them to fire the "
+                    "network change events.")
+
+    def _disable_hook(self):
+        """Destroy the link in the network dispatcher pointing to the event triggerer.
+        """
+        for dispatcher, target in self.dispatchers.items():
+            # DEBUG This test would fail on a broken link :
+            if os.path.exists(target + self.dispatcher_toggler):
+                os.remove(target + self.dispatcher_toggler)
+                print("%s dispatcher link destroyed" % dispatcher)
+            else:
+                # Report missing link only if dir is present
+                if os.path.exists(target):
+                    print("there was no %s dispatcher link" % dispatcher)
 
     def _enable_in_systemd(self):
-        self._systemctl('start')
         return self._systemctl('enable')
 
     def _disable_in_systemd(self):
-        self._systemctl('stop')
         return self._systemctl('disable')
 
     def _systemctl(self, arg):
         try:
-            return subprocess.check_output(['systemctl', arg, 'servicewall.service']).strip()
+            return subprocess.check_output(['systemctl', arg, 'servicewall.service']).decode().strip()
         except FileNotFoundError:
             raise AssertionError("'systemctl' not found in the path, is systemd installed on this machine ?")
         except subprocess.CalledProcessError:
